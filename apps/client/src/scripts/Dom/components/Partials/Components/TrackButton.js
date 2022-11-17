404430bdbc8dfbca62cad99c737bc05843bc0e00
Evan Martin <evan.martiin29@gmail.com>
2022-11-17 14:08:27 +0100

    fix(UITrack): remove active

--- apps/client/src/scripts/Dom/components/Partials/Components/TrackButton.js
+++ apps/client/src/scripts/Dom/components/Partials/Components/TrackButton.js
@@ -4,17 +4,15 @@ import { STORE_KEYS } from 'utils/constants.js';
 import trackConfig from 'utils/trackConfig.js';
 
 export default class TrackButton extends Component {
-	static tracks = new Map();
 	static activeTrackName = null;
 
 	constructor(el) {
 		super(el);
-		if ([...TrackButton.tracks].length === 0) {
+		if (TrackButton.activeTrackName === null) {
 			el.classList.add('active');
 			TrackButton.activeTrackName = el.children[0].innerText;
 			store.set(STORE_KEYS.TRACK_NAME, this.el.children[0].innerText);
 		}
-		TrackButton.tracks.set(el.children[0].innerText, el);
 		this.coverImg = document.querySelector('#track-cover-img');
 		this.pathImg = document.querySelector('#track-path-img');
 		this._updateImages();
@@ -29,7 +27,9 @@ export default class TrackButton extends Component {
 	}
 
 	_handleClick = () => {
-		TrackButton.tracks.get(TrackButton.activeTrackName)?.classList.remove('active');
+		[...this.el.parentElement.children].forEach((element) => {
+			element.classList.remove('active');
+		});
 		this.el.classList.add('active');
 		TrackButton.activeTrackName = this.el.children[0].innerText;
 		store.set(STORE_KEYS.TRACK_NAME, this.el.children[0].innerText);
